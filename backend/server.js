292a1da4b4ab449d02b6922fa6ce4b1ba3e12815
9802HEMENSAN <hmahilange9802@gmail.com>
2023-10-02 17:06:07 +0530

    MONGO_URL added to our application

--- backend/server.js
+++ backend/server.js
@@ -28,8 +28,3 @@ app.listen(port, async () => {
     }
     console.log(`App is running at http://localhost:${port}`)
 })
-
-
-
-// GET POST /user/savefeed
-// https://gnews.io/api/v4/search?q=example&lang=en&country=us&max=10&apikey=221e180a111fac6abf3e20a582138e8d
